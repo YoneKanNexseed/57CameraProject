d8f86154a9a32d7c7301910dcde43d62e33fd933
obscure723 <obscure723@yone-lab.com>
2019-11-19 14:20:24 +0800

    レイアウト追加

--- CameraProject/ViewController.swift
+++ CameraProject/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // カメラボタン
+    @IBAction func runCamera(_ sender: UIButton) {
+        
+    }
+    
+    // アルバムボタン
+    @IBAction func runAlbum(_ sender: UIButton) {
+        
+    }
+    
+    // 保存ボタン
+    @IBAction func savePhoto(_ sender: UIButton) {
+    }
+    
 }
 
